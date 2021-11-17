b71243ecf780d4a72dcbf9b5a8210d2336019803
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-11-17 20:38:35 +0700

    feat: add set_text method

--- spinner.v
+++ spinner.v
@@ -4,10 +4,15 @@ import os { read_file, dir, real_path, join_path }
 import x.json2 { raw_decode }
 import time
 
+const (
+	json_path = real_path(join_path(dir(@FILE), "./spinners_condensed.json"))
+)
+
 // shared objects must be a struct, map, or array
 struct SharedSpinner {
 mut:
     is_running bool
+    text string
 }
 
 pub struct Spinner {
@@ -17,7 +22,7 @@ mut:
 }
 
 fn get_from_json(posx string) ?([]string, i64) {
-    spinners_data := read_file(real_path(join_path(dir(@FILE), "./spinners_condensed.json"))) ?
+    spinners_data := read_file(json_path) ?
     mp_obj := raw_decode(spinners_data) ?    
     arr := mp_obj.arr().map(it.as_map())
     
@@ -35,15 +40,16 @@ fn get_from_json(posx string) ?([]string, i64) {
     return error("Couldn't find the specified spinner")
 }
 
-fn (mut self Spinner) spinner_thread(frames []string, interval i64, suffix string) {
+fn (mut self Spinner) spinner_thread(frames []string, interval i64) {
     mut index := 0
     
     for {
         frame := frames[index]
-        print(' \r $frame $suffix')
-        time.sleep(interval)
         
         rlock self.shr {
+            print(' \r $frame $self.shr.text')
+            time.sleep(interval)
+        
             if self.shr.is_running == false {
                 print('\n') // print newline before exiting
                 return
@@ -57,23 +63,30 @@ fn (mut self Spinner) spinner_thread(frames []string, interval i64, suffix strin
     }
 }
 
-pub fn (mut self Spinner) start(_posx string, suffix string) ? {
+pub fn (mut self Spinner) start(_posx string, text string) ? {
     lock self.shr {
         if self.shr.is_running == true {
-            return error("Spinner is already running.")
+            return
         }
         
         self.shr.is_running = true
+        self.shr.text = text
     }
-	
+    
     frames, interval := get_from_json(_posx) ?
-    self.running_thread = go self.spinner_thread(frames, interval, suffix)
+    self.running_thread = go self.spinner_thread(frames, interval)
 }
 
-pub fn (mut self Spinner) stop() ? {
+pub fn (mut self Spinner) set_text(new_text string) {
+    lock self.shr {
+        self.shr.text = new_text
+    }
+}
+
+pub fn (mut self Spinner) stop() {
     lock self.shr {
         if self.shr.is_running == false {
-            return error("Spinner is not running.")
+            return
         }
         
         self.shr.is_running = false
@@ -83,6 +96,6 @@ pub fn (mut self Spinner) stop() ? {
 }
 
 // TODO: add success method
-// pub fn success(suffix string) ? {
+// pub fn success(text string) ? {
 // 
 // }
