94cd981d03908d554e643096a40117a4d4209087
null <60427892+vierofernando@users.noreply.github.com>
2021-11-17 20:03:52 +0700

    feat: make everything object-oriented

--- spinner.v
+++ spinner.v
@@ -4,12 +4,19 @@ import os { read_file, dir, real_path, join_path }
 import x.json2 { raw_decode }
 import time
 
-struct Spinner {
-    frames []string // frames of the spinner
-    interval i64 // in milliseconds
+// shared objects must be a struct, map, or array
+struct SharedSpinner {
+mut:
+    is_running bool
 }
 
-fn get_from_json(posx string) ?Spinner {
+pub struct Spinner {
+mut:
+    shr shared SharedSpinner
+    running_thread thread
+}
+
+fn get_from_json(posx string) ?([]string, i64) {
     spinners_data := read_file(real_path(join_path(dir(@FILE), "./spinners_condensed.json"))) ?
     mp_obj := raw_decode(spinners_data) ?    
     arr := mp_obj.arr().map(it.as_map())
@@ -21,36 +28,61 @@ fn get_from_json(posx string) ?Spinner {
             f := mp['frames'] ?
             i := mp['interval'] ?
         
-            return Spinner {
-                frames: f.arr().map(it.str())
-                interval: i.int() * time.millisecond
-            }
+            return f.arr().map(it.str()), i.int() * time.millisecond
         }
     }
     
     return error("Couldn't find the specified spinner")
 }
 
-fn print_spinner(spin Spinner, suffix string) {
-    for frame in spin.frames {
+fn (mut self Spinner) spinner_thread(frames []string, interval i64, suffix string) {
+    mut index := 0
+    
+    for {
+        frame := frames[index]
         print(' \r $frame $suffix')
-
-        time.sleep(spin.interval)
+        time.sleep(interval)
+        
+        rlock self.shr {
+            if self.shr.is_running == false {
+                print('\n') // print newline before exiting
+                return
+            }
+        }
+        
+        index++
+        if index == frames.len {
+            index = 0
+        }
     }
+}
 
+pub fn (mut self Spinner) start(_posx string, suffix string) ? {
+    lock self.shr {
+        if self.shr.is_running == true {
+            return error("Spinner is already running.")
+        }
+        
+        self.shr.is_running = true
+        
+        frames, interval := get_from_json(_posx) ?
+        self.running_thread = go self.spinner_thread(frames, interval, suffix)
+    }
+}
+
+pub fn (mut self Spinner) stop() ? {
+    lock self.shr {
+        if self.shr.is_running == false {
+            return error("Spinner is not running.")
+        }
+        
+        self.shr.is_running = false
+    }
     
+    self.running_thread.wait()
 }
 
-pub fn spin(_posx string, suffix string) ? {
-    data := get_from_json(_posx) ?
-    h := go print_spinner(data, suffix)
-     
-    h.wait()
-    
-
-}
-
-
-pub fn success(suffix string) ? {
-
-}
+// TODO: add success method
+// pub fn success(suffix string) ? {
+// 
+// }
