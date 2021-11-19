34cf1e7f77f6e282f297df449ea573e29dcddbf3
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-11-19 15:08:58 +0700

    refactor: remove unused file

--- comp_windows.c.v
+++
@@ -1,9 +0,0 @@
-module spinners
-
-#flag -I @VMODROOT
-#flag @VMODROOT/spinner.o
-#include "spinner.h"
-
-// windows functions
-fn C.print_spinner_text(int, &char, &char, int, int)
-fn C.print_post_exit(int, int, int, &char, int)
